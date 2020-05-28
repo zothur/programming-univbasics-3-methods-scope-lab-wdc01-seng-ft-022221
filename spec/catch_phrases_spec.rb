f0124f3fc765e105283af0415ce4f23748b895b3
taoliu12 <taoliudesign@gmail.com>
2020-05-28 14:16:39 -0700

    fix instructions and spec

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -19,7 +19,7 @@ describe "link" do
 end
 
 describe "all_phrases" do
-  it "takes in an argument and puts out the catch phrase" do
+  it "puts out all of the previous catch phrases" do
     expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
     expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
     expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
